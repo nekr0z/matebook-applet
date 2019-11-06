51201b21869e3418284dfe1fa0d2a8a197220359
Evgeny Kuznetsov <evgeny@kuznetsov.md>
2019-11-06 19:03:47 +0300

    refactor(main): code readability

--- main.go
+++ main.go
@@ -19,17 +19,19 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
 	"github.com/BurntSushi/toml"
 	"github.com/andlabs/ui"
 	"github.com/cloudfoundry/jibber_jabber"
 	"github.com/getlantern/systray"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
 	"golang.org/x/text/language"
-	"io"
-	"io/ioutil"
-	"log"
-	"os"
-	"path/filepath"
 )
 
 var (
@@ -52,108 +54,14 @@ var (
 	}
 )
 
-func logInit(
-	traceHandle io.Writer,
-	infoHandle io.Writer,
-	warningHandle io.Writer,
-	errorHandle io.Writer) {
-	logTrace = log.New(traceHandle,
-		"TRACE: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-
-	logInfo = log.New(infoHandle,
-		"INFO: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-
-	logWarning = log.New(warningHandle,
-		"WARNING: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-
-	logError = log.New(errorHandle,
-		"ERROR: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-}
-
 func main() {
-	verbose := flag.Bool("v", false, "be verbose")
-	verboseMore := flag.Bool("vv", false, "be very verbose")
-	flag.StringVar(&iconPath, "icon", "", "path of a custom icon to use")
-	flag.BoolVar(&config.wait, "wait", false, "wait for driver to set battery thresholds (obsolete)")
-	flag.BoolVar(&saveValues, "s", false, "save values for persistence (deprecated)") // TODO: remove in v3
-	flag.BoolVar(&noSaveValues, "n", false, "do not save values")
-	flag.BoolVar(&config.useScripts, "r", true, "use fnlock and batpro scripts if all else fails") // TODO: default to false in v3
-	flag.BoolVar(&config.windowed, "w", false, "windowed mode")
-	flag.Parse()
-
-	switch {
-	case *verbose:
-		logInit(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
-	case *verboseMore:
-		logInit(os.Stdout, os.Stdout, os.Stdout, os.Stderr)
-	default:
-		logInit(ioutil.Discard, ioutil.Discard, os.Stdout, os.Stderr)
-	}
-
-	// i18n init
-	bundle := i18n.NewBundle(language.English)
-	bundle.RegisterUnmarshalFunc("toml", toml.Unmarshal)
-	bundle.MustAddMessages(language.English, messages...)
-	if tr, err := assets.Open("/translations"); err == nil {
-		if files, err := tr.Readdir(-1); err == nil {
-			for _, file := range files {
-				if ok, err := filepath.Match("active.*.toml", file.Name()); ok && err == nil {
-					f, err := assets.Open(filepath.Join("/translations", file.Name()))
-					if err == nil {
-						b, err := ioutil.ReadAll(f)
-						if err == nil {
-							_, err := bundle.ParseMessageFileBytes(b, file.Name())
-							if err != nil {
-								logError.Printf("error reading translation file %s: %s", file.Name(), err)
-							}
-						}
-						f.Close()
-					}
-				}
-			}
-		}
-		tr.Close()
-	}
-
-	lang, err := jibber_jabber.DetectIETF()
-	if err != nil {
-		logWarning.Println("could not detect locale")
-	}
-
-	localizer = i18n.NewLocalizer(bundle, lang)
-	logTrace.Println(localizer.MustLocalize(&i18n.LocalizeConfig{DefaultMessage: &i18n.Message{ID: "DetectedLocale", Other: "detected locale: {{.Locale}}"}, TemplateData: map[string]interface{}{"Locale": lang}}))
+	i18nInit()
+	parseFlags()
 
 	logInfo.Println(localizer.MustLocalize(&i18n.LocalizeConfig{DefaultMessage: &i18n.Message{ID: "AppletVersion", Other: "matebook-applet version {{.Version}}"}, TemplateData: map[string]interface{}{"Version": version}}))
 
-	// need to find working fnlock interface (if any)
-	for _, fnlck := range fnlockEndpoints {
-		_, err := fnlck.get()
-		if err != nil {
-			continue
-		}
-		config.fnlock = fnlck
-		if fnlck.isWritable() {
-			logInfo.Println(localizer.MustLocalize(&i18n.LocalizeConfig{DefaultMessage: &i18n.Message{ID: "FoundFnlock", Other: "Found writable fnlock endpoint, will use it"}}))
-			break
-		}
-	}
-
-	// need to find working threshold interface (if any)
-	for _, thresh := range threshEndpoints {
-		_, _, err := thresh.get()
-		if err != nil {
-			continue
-		}
-		config.thresh = thresh
-		if thresh.isWritable() {
-			logInfo.Println(localizer.MustLocalize(&i18n.LocalizeConfig{DefaultMessage: &i18n.Message{ID: "FoundBattery", Other: "Found writable battery thresholds endpoint, will use it"}}))
-			break
-		}
-	}
+	findFnlock()
+	findThresh()
 
 	if saveValues {
 		logWarning.Println(localizer.MustLocalize(&i18n.LocalizeConfig{DefaultMessage: &i18n.Message{ID: "OptionSDeprecated", Other: "-s option is deprecated, applet is now saving values for persistence by default"}}))
@@ -182,4 +90,110 @@ func main() {
 	} else {
 		logError.Println(localizer.MustLocalize(&i18n.LocalizeConfig{DefaultMessage: &i18n.Message{ID: "NothingToWorkWith", Other: "Neither a supported version of Huawei-WMI driver, nor any of the required scripts are properly installed, see README.md#installation-and-setup for instructions"}}))
 	}
+}
+
+// findFnlock finds working fnlock interface (if any)
+func findFnlock() {
+	for _, fnlck := range fnlockEndpoints {
+		_, err := fnlck.get()
+		if err != nil {
+			continue
+		}
+		config.fnlock = fnlck
+		if fnlck.isWritable() {
+			logInfo.Println(localizer.MustLocalize(&i18n.LocalizeConfig{DefaultMessage: &i18n.Message{ID: "FoundFnlock", Other: "Found writable fnlock endpoint, will use it"}}))
+			break
+		}
+	}
+}
+
+// findThresh finds working threshold interface (if any)
+func findThresh() {
+	for _, thresh := range threshEndpoints {
+		_, _, err := thresh.get()
+		if err != nil {
+			continue
+		}
+		config.thresh = thresh
+		if thresh.isWritable() {
+			logInfo.Println(localizer.MustLocalize(&i18n.LocalizeConfig{DefaultMessage: &i18n.Message{ID: "FoundBattery", Other: "Found writable battery thresholds endpoint, will use it"}}))
+			break
+		}
+	}
+}
+
+func parseFlags() {
+	verbose := flag.Bool("v", false, localizer.MustLocalize(&i18n.LocalizeConfig{DefaultMessage: &i18n.Message{ID: "FlagV", Other: "be verbose"}}))
+	verboseMore := flag.Bool("vv", false, localizer.MustLocalize(&i18n.LocalizeConfig{DefaultMessage: &i18n.Message{ID: "FlagVV", Other: "be very verbose"}}))
+	flag.StringVar(&iconPath, "icon", "", localizer.MustLocalize(&i18n.LocalizeConfig{DefaultMessage: &i18n.Message{ID: "FlagIcon", Other: "path of a custom icon to use"}}))
+	flag.BoolVar(&config.wait, "wait", false, localizer.MustLocalize(&i18n.LocalizeConfig{DefaultMessage: &i18n.Message{ID: "FlagWait", Other: "wait for driver to set battery thresholds (obsolete)"}}))
+	flag.BoolVar(&saveValues, "s", false, localizer.MustLocalize(&i18n.LocalizeConfig{DefaultMessage: &i18n.Message{ID: "FlagS", Other: "save values for persistence (deprecated)"}})) // TODO: remove in v3
+	flag.BoolVar(&noSaveValues, "n", false, localizer.MustLocalize(&i18n.LocalizeConfig{DefaultMessage: &i18n.Message{ID: "FlagN", Other: "do not save values"}}))
+	flag.BoolVar(&config.useScripts, "r", true, localizer.MustLocalize(&i18n.LocalizeConfig{DefaultMessage: &i18n.Message{ID: "FlagR", Other: "use fnlock and batpro scripts if all else fails"}})) // TODO: default to false in v3
+	flag.BoolVar(&config.windowed, "w", false, localizer.MustLocalize(&i18n.LocalizeConfig{DefaultMessage: &i18n.Message{ID: "FlagW", Other: "windowed mode"}}))
+	flag.Parse()
+
+	switch {
+	case *verbose:
+		logInit(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
+	case *verboseMore:
+		logInit(os.Stdout, os.Stdout, os.Stdout, os.Stderr)
+	default:
+		logInit(ioutil.Discard, ioutil.Discard, os.Stdout, os.Stderr)
+	}
+}
+
+func logInit(
+	traceHandle io.Writer,
+	infoHandle io.Writer,
+	warningHandle io.Writer,
+	errorHandle io.Writer) {
+	logTrace = log.New(traceHandle,
+		"TRACE: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	logInfo = log.New(infoHandle,
+		"INFO: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	logWarning = log.New(warningHandle,
+		"WARNING: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	logError = log.New(errorHandle,
+		"ERROR: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func i18nInit() {
+	bundle := i18n.NewBundle(language.English)
+	bundle.RegisterUnmarshalFunc("toml", toml.Unmarshal)
+	bundle.MustAddMessages(language.English, messages...)
+	if tr, err := assets.Open("/translations"); err == nil {
+		if files, err := tr.Readdir(-1); err == nil {
+			for _, file := range files {
+				if ok, err := filepath.Match("active.*.toml", file.Name()); ok && err == nil {
+					f, err := assets.Open(filepath.Join("/translations", file.Name()))
+					if err == nil {
+						b, err := ioutil.ReadAll(f)
+						if err == nil {
+							_, err := bundle.ParseMessageFileBytes(b, file.Name())
+							if err != nil {
+								fmt.Printf("error reading translation file %s: %s", file.Name(), err)
+							}
+						}
+						f.Close()
+					}
+				}
+			}
+		}
+		tr.Close()
+	}
+
+	lang, err := jibber_jabber.DetectIETF()
+	if err != nil {
+		fmt.Println("could not detect locale")
+	}
+
+	localizer = i18n.NewLocalizer(bundle, lang)
 }
