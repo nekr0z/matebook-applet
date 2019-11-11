210b2380ac867d2e137d7f550e132124bf1d37d4
Evgeny Kuznetsov <evgeny@kuznetsov.md>
2019-11-11 22:37:13 +0300

    script(release): strip release script of unnecessary checks

--- release.go
+++ release.go
@@ -19,14 +19,12 @@ package main
 
 import (
 	"bytes"
-	"flag"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"os/user"
 	"path/filepath"
-	"regexp"
 	"strings"
 )
 
@@ -35,7 +33,6 @@ var (
 )
 
 func main() {
-	flag.Parse()
 	if os.Getenv("GITHUB_TOKEN") == "" {
 		log.Fatalln("github token not available, can't work")
 	}
@@ -43,23 +40,9 @@ func main() {
 		log.Fatalln("github user not set, can't work")
 	}
 
-	var version string
-	if flag.NArg() == 0 {
-		// release latest version
-		res, err := getString("git", "describe")
-		if err != nil {
-			log.Fatalln(err)
-		}
-		r := strings.Split(res, "-")
-		version = r[0]
-	} else {
-		version = flag.Arg(0)
-	}
-
-	// check version format
-	re := regexp.MustCompile(`^v?[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$`)
-	if !re.MatchString(version) {
-		log.Fatalln("version", version, "doesn't make sense, giving up!")
+	version, err := getString("git", "describe")
+	if err != nil {
+		log.Fatalln(err)
 	}
 
 	log.Println("Trying to release version", version)
@@ -70,16 +53,20 @@ func process(version string) {
 	gitVersion := version
 	version = strings.TrimPrefix(version, "v")
 
+	release := true
 	// check if corresponding tag even exists
 	res, err := getString("git", "ls-remote", "origin", gitVersion)
 	if err != nil || res == "" {
-		log.Fatalln("Tag doesn't seem to exist, giving up.")
+		fmt.Println("Tag doesn't seem to exist, won't be releasing.")
+		release = false
 	}
 
+	releaseGithub := true
 	// check if this version is already released
 	res, err = getString("gothub", "info", "-r", githubRepo, "-t", gitVersion)
 	if err == nil || res != "error: could not find the release corresponding to tag "+gitVersion {
-		log.Fatalln("Something wrong. Already released? Giving up.")
+		fmt.Println("Something wrong. Already released? Won't be pushing to Github.")
+		releaseGithub = false
 	}
 
 	// build
@@ -90,57 +77,64 @@ func process(version string) {
 		log.Fatalln(err)
 	}
 
-	debFilename := "matebook-applet_" + version + "_amd64.deb"
-
-	// get release description from tag
-	desc, err := getString("git", "tag", "-ln", "--format=%(contents)", gitVersion)
+	debFilenames, err := filepath.Glob("*.deb")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	descStrings := strings.Split(desc, "\n")
-	for i, descString := range descStrings {
-		if descString == "-----BEGIN PGP SIGNATURE-----" {
-			descStrings = descStrings[:i]
-			break
+
+	if release && releaseGithub {
+		// get release description from tag
+		desc, err := getString("git", "tag", "-ln", "--format=%(contents)", gitVersion)
+		if err != nil {
+			log.Fatalln(err)
 		}
-	}
-	desc = strings.Join(descStrings, "\n")
+		descStrings := strings.Split(desc, "\n")
+		for i, descString := range descStrings {
+			if descString == "-----BEGIN PGP SIGNATURE-----" {
+				descStrings = descStrings[:i]
+				break
+			}
+		}
+		desc = strings.Join(descStrings, "\n")
 
-	// release version
-	args := []string{
-		"release",
-		"-r", githubRepo,
-		"-t", gitVersion,
-		"-n", version,
-		"-d", desc,
-	}
-	cmd = exec.Command("gothub", args...)
-	if err := cmd.Run(); err != nil {
-		log.Fatalln(err)
-	}
-
-	// release packages
-	fileNames := []string{
-		"matebook-applet-amd64-" + gitVersion + ".tar.gz",
-		debFilename,
-	}
-	for _, fileName := range fileNames {
+		// release version
 		args := []string{
-			"upload",
+			"release",
 			"-r", githubRepo,
 			"-t", gitVersion,
-			"-n", fileName,
-			"-f", fileName,
+			"-n", version,
+			"-d", desc,
 		}
-		cmd := exec.Command("gothub", args...)
+		cmd = exec.Command("gothub", args...)
 		if err := cmd.Run(); err != nil {
-			fmt.Println("failed to upload", fileName)
+			log.Fatalln(err)
 		}
-		fmt.Println(fileName, "uploaded successfully")
+
+		// release packages
+		fileNames := []string{
+			"matebook-applet-amd64-" + gitVersion + ".tar.gz",
+		}
+		fileNames = append(fileNames, debFilenames...)
+		for _, fileName := range fileNames {
+			args := []string{
+				"upload",
+				"-r", githubRepo,
+				"-t", gitVersion,
+				"-n", fileName,
+				"-f", fileName,
+			}
+			cmd := exec.Command("gothub", args...)
+			if err := cmd.Run(); err != nil {
+				fmt.Println("failed to upload", fileName)
+			}
+			fmt.Println(fileName, "uploaded successfully")
+		}
 	}
 
 	// update debian repo
-	updateRepo(debFilename)
+	if release {
+		updateRepo(debFilenames)
+	}
 }
 
 func getString(c string, a ...string) (string, error) {
@@ -150,8 +144,8 @@ func getString(c string, a ...string) (string, error) {
 }
 
 // use aptly and rsync to update debian repo
-func updateRepo(filename string) {
-	if updateLocalRepo(filename) {
+func updateRepo(filenames []string) {
+	if updateLocalRepo(filenames) {
 		usr, err := user.Current()
 		if err != nil {
 			log.Fatalln(err)
@@ -167,13 +161,15 @@ func updateRepo(filename string) {
 }
 
 // update aptly local repo
-func updateLocalRepo(filename string) bool {
-	cmd := exec.Command("aptly", "repo", "add", "matebook-applet", filename)
-	if err := cmd.Run(); err != nil {
-		fmt.Println("failed to add", filename, "to local aptly repo")
-		return false
+func updateLocalRepo(filenames []string) bool {
+	for _, filename := range filenames {
+		cmd := exec.Command("aptly", "repo", "add", "matebook-applet", "*.deb")
+		if err := cmd.Run(); err != nil {
+			fmt.Println("failed to add", filename, "to local aptly repo")
+			return false
+		}
 	}
-	cmd = exec.Command("aptly", "publish", "repo", "matebook-applet")
+	cmd := exec.Command("aptly", "publish", "repo", "matebook-applet")
 	if err := cmd.Run(); err != nil {
 		fmt.Println("failed to locally publish repo")
 		return false
