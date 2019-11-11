8933e5c6f8ea99474c3ea9ff56edb40a1e27650c
Evgeny Kuznetsov <evgeny@kuznetsov.md>
2019-11-11 18:37:09 +0300

    script: deployment fixes

--- build.go
+++ build.go
@@ -283,7 +283,9 @@ func setFileTime(f string, t int64) {
 }
 
 func signFile(f string, k string) {
-	cmd := exec.Command("gpg", "--detach-sign", "--yes", "--passphrase", os.Getenv("GPG_PASSPHRASE"), "--pinentry-mode", "loopback", "-u", k, f)
+	cmd := exec.Command("gpg", "--detach-sign", "--yes", "--passphrase", os.Getenv("GPG_PASSPHRASE"), "--pinentry-mode", "loopback", "-a", "-u", k, f)
+	cmd.Stderr = os.Stdout
+	cmd.Stdout = os.Stdout
 	if err := cmd.Run(); err != nil {
 		fmt.Println("signing", f, "failed")
 		filename = filename + "-unsigned"
