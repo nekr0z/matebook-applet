5544f5af013b004ae1174bc7c7d57928b342241f
Evgeny Kuznetsov <evgeny@kuznetsov.md>
2021-01-23 20:36:04 +0300

    fix tests

---
+++ main_test.go
@@ -0,0 +1,27 @@
+// Copyright (C) 2021 Evgeny Kuznetsov (evgeny@kuznetsov.md)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along tihe this program. If not, see <https://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"testing"
+)
+
+// the following declaration is required to init the testing package
+// before flags are parsed
+var _ = func() bool {
+	testing.Init()
+	return true
+}()
